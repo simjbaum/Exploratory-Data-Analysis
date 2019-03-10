3f3292fc2ed40f7138f93944d284a29be20baf29
simjbaum <sim.j.baum@gmail.com>
2019-03-10 19:54:33 +0100

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,24 @@
+# R code to make the plots for the assignment 1
+library(dplyr)
+library(chron)
+
+# read in the data
+data <- data.table::fread(file = "~/Data_Science/exploratory_data_analysis/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+
+# format the Date format 
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+# subset the data 
+data_sub<-filter(data, Date == "2007-02-01" | Date ==  "2007-02-02")
+
+#format time 
+data_sub$Time <- strptime(paste(data_sub$Date, data_sub$Time), "%Y-%m-%d %H:%M:%S")
+
+
+
+### plotting graphs ### 
+
+#plot2
+png(filename = "~/Data_Science/exploratory_data_analysis/plot2.png")
+with(data_sub, plot(Time, Global_active_power, type="l", ylab  ="Global Active Power (kilowatts)", xlab=""))
+dev.off()
